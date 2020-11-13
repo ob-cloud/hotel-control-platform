a136d0f62aabaf2fd9285253cbb2a2ba3046c7e3
eamiear <justcall_me_k@163.com>
2020-11-13 17:11:49 +0800

    昂宝中控平台

---
+++ src/views/user/Login.vue
@@ -0,0 +1,17 @@
+<script>
+// import SimpleLogin from './components/SimpleLogin'
+import ProLogin from './components/ProLogin'
+// import MultipleLogin from './components/MultiLogin'
+import config from '@/config/defaultSettings'
+export default {
+  name: 'Login',
+  components: { ProLogin },
+  beforeCreate () {
+  },
+  render () {
+    return (
+      config.pageOption.loginMode === 'simple' ? (<ProLogin />) : (<span></span>)
+    )
+  }
+}
+</script>
